c9ce0507eeaa7f57d888151088107f1b19ea6b39
Nuttapong Klaokliang <nuttapong95.klng@gmail.com>
2022-10-12 12:25:54 +0700

    Update user model

--- user/model.go
+++ user/model.go
@@ -1,17 +1,13 @@
 package user
 
 type User struct {
-	ID                string `json:"id" db:"id,type:uuid;primary_key;default;uuid_generate_v4()"`
-	Email             string `json:"email" db:"email"`
-	Password          string `json:"password" db:"password"`
-	FirstName         string `json:"first_name" db:"first_name"`
-	LastName          string `json:"last_name" db:"last_name"`
-	IDCardFile        string `json:"id_card_file" db:"id_card_file"` //TODO: file upload handling
-	IDCardNumber      string `json:"id_card_number" db:"id_card_number"`
-	IDCardLaserNumber string `json:"id_card_laser_number" db:"id_card_laser_number"`
-	DateOfBirth       string `json:"date_of_birth" db:"date_of_birth"`
-	CreatedAt         string `json:"created_at" db:"created_at"`
-	UpdatedAt         string `json:"updated_at" db:"updated_at"`
+	ID        string `json:"id" db:"id,type:uuid;primary_key;default;uuid_generate_v4()"`
+	Email     string `json:"email" db:"email"`
+	Password  string `json:"password" db:"password"`
+	FirstName string `json:"first_name" db:"first_name"`
+	LastName  string `json:"last_name" db:"last_name"`
+	CreatedAt string `json:"created_at" db:"created_at"`
+	UpdatedAt string `json:"updated_at" db:"updated_at"`
 }
 
 type LoginInfo struct {
